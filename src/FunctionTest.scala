183488addf60a6e8ccc808d0afc1c535a00beaac
LUJUHUI <brucelujuhui@163.com>
2018-07-25 16:17:02 +0800

    update

--- src/FunctionTest.scala
+++ src/FunctionTest.scala
@@ -1,10 +1,19 @@
 object FunctionTest {
-  def main(args: Array[String]): Unit = {
-    def f1(f: (Int, Int) => Int) = f(2, 3)
+  def f1(f: (Int, Int) => Int) = f(2, 3)
 
-    var h1 = (x: Int, y: Int) => {
-      x * y
-    }
-    println(f1(h1))
+  def f2(f: (Int, Int) => Int) = f(5, 6)
+
+  var h1 = (x: Int, y: Int) => {
+    x * y
+  }
+
+  var h2 = (x: Int, y: Int) => {
+    x / y
+  }
+
+  def main(args: Array[String]): Unit = {
+    var m1 = f1(h1)
+    var m2 = f2(h2)
+    println(m1, m2)
   }
 }
